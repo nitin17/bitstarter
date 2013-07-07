72b8fbf97aaaf874a263c84472860396658c81b2
Nitin kakkar <nitinkakkar17@gmail.com>
2013-07-07 10:08:06 +0530

    hello world from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  var result = data.toString();
+  response.send(result);
+});
 });
 
 var port = process.env.PORT || 5000;
