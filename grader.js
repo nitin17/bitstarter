0eb7e0f382e9e103a6626ec7d5494eeb388eed31
Nitin kakkar <nitinkakkar17@gmail.com>
2013-07-15 09:34:26 +0530

    homework 4 part 1

--- grader.js
+++ grader.js
@@ -21,12 +21,15 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var util = require('util');
+var rest = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+var urlfile = "url.html";
+var URL_DEFAULT = "http://www.google.com";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -65,10 +68,33 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url> ', 'URL to index.html', URL_DEFAULT)
         .parse(process.argv);
+    
+    if(program.url){
+	rest.get(program.url).on('complete', function(result) {
+	    if (result instanceof Error) {
+		console.log('Error: ' + result.message);
+		 
+	    } else {
+		var html = cheerio.load(result);
+		var checks = loadChecks(program.checks).sort();
+		var out = {};
+		for (var ii in checks){
+		    var present = html(checks[ii]).length > 0;
+		    out[checks[ii]] = present;
+		}
+		var outJson = JSON.stringify(out , null, 4);
+		console.log(outJson);
+	    }
+	});
+
+    }
+    else {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    console.log(outJson);}
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
